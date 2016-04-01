46a209c0e62329755083529468a8065892befcb5
Andrey Chausenko <Andrey.Chausenko@team.telstra.com>
2016-04-01 13:16:06 +1100

    Initial commit

---
+++ service.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var port = process.env.SERVICE_PORT || 3000;
+var serviceName = process.env.SERVICE_NAME;
+
+app.get('/', function (req, res) {
+    res.send('Hello World!\nI am ' + serviceName + '\n');
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
