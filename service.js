902f79ee4a87cdc69d276ec0cc92df7cd6ba6c66
Andrey Chausenko <Andrey.Chausenko@team.telstra.com>
2016-04-28 10:15:25 +1000

    Added request details logging

--- service.js
+++ service.js
@@ -4,6 +4,11 @@ var port = process.env.SERVICE_PORT || 3000;
 var serviceName = process.env.SERVICE_NAME;
 
 app.get('/', function (req, res) {
+    console.log(JSON.stringify({
+        time: Date.now(),
+        ip: req.connection.remoteAddress,
+        headers: req.headers
+    }, null, 3));
     res.send('Hello World!\nI am ' + serviceName + '!\n');
 });
 
